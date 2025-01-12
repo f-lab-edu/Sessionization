70be866044e79c7c62b81e36ed0ad2e666738686
dodorip <dodorippp@gmail.com>
2025-01-12 19:40:53 +0900

    refactor: DAG 파일의 코드 정리

--- airflow/dags/hourly_batch_sessionization.py
+++ airflow/dags/hourly_batch_sessionization.py
@@ -1,7 +1,9 @@
 from datetime import datetime, timedelta
+from pendulum import timezone
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
+# 기본 설정
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -9,38 +11,34 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
 }
 
-# Spark 설정 변수
-SPARK_JOB = {
-    "task_id": "sessionization",
-    "application": "/Users/doyeonpyun/Desktop/Sessionization/src/main/Sessionization.py",
-    "conn_id": "local_spark",
+# Spark 설정
+spark_config = {
     "application_args": [
-        "{{ ds }}",
-        "{{ logical_date.strftime('%H') }}"
+        "{{ ds }}",  # 실행 날짜
+        "{{ logical_date.strftime('%H') }}"  # 실행 시간
     ],
-    # 추가 설정
-    # 'conf': {  # 필요 시 추가
-    #     "spark.yarn.maxAppAttempts": "1",
-    #     "spark.yarn.executor.memoryOverhead": "5120"
-    # },
-    # 'driver_memory': "3g",  # 필요 시 추가
-    # 'executor_cores': 10,  # 필요 시 추가
-    # 'num_executors': 10,  # 필요 시 추가
-    # 'executor_memory': '5g',  # 필요 시 추가
-    # 'keytab': '/keytab/path',  # Kerberos 인증 필요 시 추가
-    # 'principal': '{keytab.principal}',  # Kerberos 인증 필요 시 추가
-    # 'java_class': '{jar파일 안에 포함된 main class}'  # JAR 실행 시 필요
+    # 필요 시 아래 주석 해제하여 추가 설정
+    # 'driver_memory': "3g",
+    # 'executor_cores': 4,
+    # 'executor_memory': '4g',
 }
 
+# DAG 정의
 with DAG(
     dag_id='hourly_batch_sessionization',
     default_args=default_args,
     description='Add session_id column',
-    schedule_interval=None,  # 수동 실행
-    start_date=datetime(2025, 1, 1),
+    schedule_interval='@hourly',  # 매 시간 실행
+    start_date=datetime(2019, 10, 10, tzinfo=timezone("Asia/Seoul")),  # 시작 시간 (KST 고정)
     catchup=False,
 ) as dag:
 
-    submit_main_task = SparkSubmitOperator(**SPARK_JOB)
+    # Spark 작업 정의
+    submit_main_task = SparkSubmitOperator(
+        task_id="sessionization",
+        conn_id="local_spark",
+        application="/Users/doyeonpyun/Desktop/Sessionization/src/main/Sessionization.py",  # 스크립트 경로
+        **spark_config
+    )
 
     submit_main_task
